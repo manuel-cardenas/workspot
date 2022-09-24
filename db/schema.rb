a400997b5cf1ca687e56e69d51393d10c36e0221
Juan Giraldo <JUSEGIDU@HOTMAIL.COM>
2022-09-24 16:24:14 -0500

    Se eliminó role del user-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_202526) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_211422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_202526) do
     t.string "last_name"
     t.string "phone_number"
     t.string "dni"
-    t.boolean "role"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
