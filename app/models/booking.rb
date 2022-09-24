6e0997d11c522a45b1c60b1a4f2a1b8b4d00ed24
Juan Giraldo <JUSEGIDU@HOTMAIL.COM>
2022-09-24 17:00:27 -0500

    Se introdujeron las validaciones en los modelos

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :place
   belongs_to :user
+  validates :check_in, :check_out, presence: true
+  validates :check_out, comparison: { greater_than: :check_in }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
   has_many :places
+  validates :first_name, :last_name, :phone_number, :dni, presence: true
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :dni, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
